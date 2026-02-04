56c38ffd206e2c032b417ab03ee417eb91c07873
yelmach <yassineelmach0@gmail.com>
2026-02-04 22:28:28 +0100

    initialize the project

---
+++ backend/gateway-service/src/test/java/ecom/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ecom.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
