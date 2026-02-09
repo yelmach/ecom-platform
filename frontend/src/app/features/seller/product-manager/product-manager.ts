e1cfa5ad85be26e3198656b60debc5c9af5cf79b
yelmach <yassineelmach0@gmail.com>
2026-02-09 10:16:37 +0100

    structure frontend app

---
+++ frontend/src/app/features/seller/product-manager/product-manager.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-manager',
+  imports: [],
+  templateUrl: './product-manager.html',
+  styleUrl: './product-manager.scss',
+})
+export class ProductManager {
+
+}
