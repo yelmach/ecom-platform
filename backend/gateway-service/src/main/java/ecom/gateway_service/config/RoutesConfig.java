a6d04a2ffaef670d6cf84e15e2afd9e3e6001491
sbeytour <beytour.safae@gmail.com>
2026-02-09 17:56:32 +0100

    config routes to apply the authfilter

--- backend/gateway-service/src/main/java/ecom/gateway_service/config/RoutesConfig.java
+++ backend/gateway-service/src/main/java/ecom/gateway_service/config/RoutesConfig.java
@@ -8,14 +8,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RoutesConfig {
 
+    private final AuthenticationFilter authenticationFilter;
+
+    public RoutesConfig(AuthenticationFilter authenticationFilter) {
+        this.authenticationFilter = authenticationFilter;
+    }
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("user-service", r -> r.path("/users/**")
+                .route("user-service", r -> r.path("/auth/**", "/me/**")
+                        .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
                         .uri("lb://USER-SERVICE"))
                 .route("product-service", r -> r.path("/products/**")
+                        .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
                         .uri("lb://PRODUCT-SERVICE"))
                 .route("media-service", r -> r.path("/media/**")
+                        .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
                         .uri("lb://MEDIA-SERVICE"))
                 .build();
     }
