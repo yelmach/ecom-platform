1d362c88b924d24b274510143a76bec3e507fb2e
yelmach <yassineelmach0@gmail.com>
2026-02-09 13:04:10 +0100

    login page template

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     path: 'register',
     loadComponent: () => import('./features/auth/register/register').then((m) => m.Register),
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+  },
 ];
--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  readonly hidePassword = signal(true);
 
+  readonly loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  fieldErrors = signal<{ [key: string]: string }>({});
+
+  togglePasswordVisibility(): void {
+    this.hidePassword.update((hide) => !hide);
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Login Data:', this.loginForm.value);
+  }
+
+  getErrorMessage(fieldName: string): string {
+    const backendError = this.fieldErrors()[fieldName];
+    if (backendError) {
+      return backendError;
+    }
+
+    const field = this.loginForm.get(fieldName);
+
+    if (!field || field.valid || field.untouched) {
+      return '';
+    }
+
+    const errors = field.errors;
+    if (!errors) {
+      return '';
+    }
+
+    if (errors['required']) {
+      return `${fieldName} is required`;
+    }
+
+    if (errors['email']) {
+      return 'enter a valid email';
+    }
+
+    return '';
+  }
 }
