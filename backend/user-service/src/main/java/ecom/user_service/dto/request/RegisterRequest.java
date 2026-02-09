78ae146e57a28c4701d7d0bb74b3e6e79e4f728d
sbeytour <beytour.safae@gmail.com>
2026-02-09 17:29:01 +0100

    modify fields at register and auth dtos

--- backend/user-service/src/main/java/ecom/user_service/dto/request/RegisterRequest.java
+++ backend/user-service/src/main/java/ecom/user_service/dto/request/RegisterRequest.java
@@ -1,16 +1,26 @@
 package ecom.user_service.dto.request;
 
-import javax.management.relation.Role;
-
+import ecom.user_service.models.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
-        @NotBlank(message = "First name is required") String firstName,
-        @NotBlank(message = "Last name is required") String lastName,
-        @Email(message = "Invalid Email Format") @NotBlank(message = "Email is required") String email,
-        @NotBlank @Size(min = 6, max = 100) String password,
-        @NotNull(message = "You must define your role") Role role) {
+        @NotBlank(message = "Username is required")
+        @Pattern(regexp = "^[a-zA-Z0-9]{4,15}$", message = "username must be between 4 and 15 characters and contain only characters and numbers")
+        String username,
+        @Email(message = "Invalid Email Format")
+        @NotBlank(message = "Email is required")
+        String email,
+        @NotBlank
+        @Size(min = 6, max = 100)
+        String password,
+        @NotNull(message = "You must define your role")
+        Role role,
+
+        String avatarUrl
+) {
+
 }
--- backend/user-service/src/main/java/ecom/user_service/dto/response/AuthResponse.java
+++ backend/user-service/src/main/java/ecom/user_service/dto/response/AuthResponse.java
@@ -1,7 +1,9 @@
 package ecom.user_service.dto.response;
 
+import ecom.user_service.models.Role;
+
 public record AuthResponse(
         String token,
         String email,
-        String role) {
+        Role role) {
 }
