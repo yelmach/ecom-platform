99f82dda513028c437326841d832b1d4e86a69f1
yelmach <yassineelmach0@gmail.com>
2026-09-21 11:36:16 +0000

    FIX: AuthService use PasswordEncoder interface

--- backend/user-service/src/main/java/ecom/user_service/services/AuthService.java
+++ backend/user-service/src/main/java/ecom/user_service/services/AuthService.java
@@ -4,7 +4,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import ecom.user_service.dto.request.LoginRequest;
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
--- backend/user-service/src/test/java/ecom/user_service/services/AuthServiceTest.java
+++ backend/user-service/src/test/java/ecom/user_service/services/AuthServiceTest.java
@@ -16,7 +16,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import ecom.user_service.dto.request.LoginRequest;
 import ecom.user_service.dto.request.RegisterRequest;
@@ -34,7 +34,7 @@ class AuthServiceTest {
     @Mock
     private UserRepository userRepository;
     @Mock
-    private BCryptPasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
     @Mock
     private AuthenticationManager authenticationManager;
     @Mock
